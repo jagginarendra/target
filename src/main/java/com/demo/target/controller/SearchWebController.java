1b2015ab221f54ad42220773486ee4a6395435a9
jaggi <jagginarendra@gmail.com>
2020-04-13 01:14:12 +0530

    added logger

--- src/main/java/com/demo/target/controller/SearchWebController.java
+++ src/main/java/com/demo/target/controller/SearchWebController.java
@@ -2,6 +2,8 @@ package com.demo.target.controller;
 
 import com.demo.target.model.Order;
 import com.demo.target.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +15,13 @@ public class SearchWebController {
     @Autowired
     OrderService orderService;
 
+    private static final Logger logger = LoggerFactory.getLogger(SearchWebController.class);
+
     @GetMapping("/getOrder")
-    public Order getOrder(@RequestParam String orderId)
-    {
+    public Order getOrder(@RequestParam String orderId) {
+
+        logger.info("OrderId received =" + orderId);
+
         return orderService.getOrderById(orderId);
 
     }
