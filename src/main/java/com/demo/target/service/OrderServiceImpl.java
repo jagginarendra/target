9e8dd6da46bc740e9b12004a146729faa38e274f
jaggi <jagginarendra@gmail.com>
2020-04-13 15:10:02 +0530

    added logger for service class

--- src/main/java/com/demo/target/service/OrderServiceImpl.java
+++ src/main/java/com/demo/target/service/OrderServiceImpl.java
@@ -1,14 +1,20 @@
 package com.demo.target.service;
 
 import com.demo.target.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
 public class OrderServiceImpl implements OrderService {
 
 
+    private static final Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
+
     @Override
     public Order getOrderById(String orderId) {
+
+        logger.info("returing order details for {}",orderId);
         return new Order(orderId,new Date(),5000,1500);
     }
 }
